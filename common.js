af3317aefa4c0febcb15ac4c609691d60c5abb05
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2021-06-23 15:38:41 +0800

    Create common.js
    oldid=66217946

---
+++ common.js
@@ -0,0 +1,33 @@
+importScript("User:Xiplus/js/close-rrd.js");
+importScript("User:Xiplus/js/close-rfpp.js");
+importScript("User:Xiplus/js/close-vip.js");
+importScript('User:Xiplus/js/userRightsManager.js');
+importScript('User:Xiplus/js/remove-selection-from-revisiondelete.js');
+importScript("User:Former User aDB0haVymg/Gadgets/Close-DRV.js");
+importScript("User:Former User aDB0haVymg/gadgets/close-anx.js");
+importScript('User:Xiplus/js/bulletin-editor.js');
+importScript("User:Vanished user 1929210/removehiddenchars.js");
+mw.loader.load( "https://meta.wikimedia.org/w/index.php?title=User:Zhaofeng_Li/Reflinks.js&action=raw&ctype=text/javascript" );
+mw.loader.load('//meta.wikimedia.org/w/index.php?title=User:Hoo_man/smart_rollback.js&action=raw&ctype=text/javascript');
+mw.loader.load("https://en.wikipedia.org/w/index.php?title=User:Enterprisey/rename-reason-fixer.js&action=raw&ctype=text/javascript");
+importScript("User:和平奮鬥救地球/link-ts.js");
+importScript("User:Bluedeck/serve/ar-auto-s.js");
+importScript('User:Shizhao/taggeo.js');
+importScript("User:Bluedeck/serve/blib-link.js"); /*藍桌圖書館快線*/ 
+importScript("user:bluedeck/serve/blib.js"); /*貢獻藍桌圖書館*/
+
+mw.loader.load( '/w/index.php?title=m:User:Krinkle/Scripts/CVNSimpleOverlay_wiki.js&action=raw&ctype=text/javascript' )// Backlink: [[m:User:Krinkle/Scripts/CVNSimpleOverlay_wiki.js]]
+
+importScript( 'User:燃灯/invertBlock.js' );
+mw.loader.load( '/w/index.php?title=User:Chiefwei/rater/rater.js&action=raw&ctype=text/javascript' )// Backlink: [[User:Chiefwei/rater/rater.js]] 
+mw.loader.load( '/w/index.php?title=User:Vozhuo/Tool/MOSNUM_dates.js&action=raw&ctype=text/javascript' )// Backlink: [[User:Vozhuo/Tool/MOSNUM_dates.js]] 
+importStylesheet( 'User:痛/common.css' );
+importScript("User:94rain/js/Gadget-afchelper.js");
+// 任性地换成燃灯版 1M logo（同时支持vector和monobook皮肤）
+importScript( 'User:痛/1M logo.js' );
+
+mw.loader.load( '/w/index.php?title=User:Lopullinen/gadgets/permanentlink.js&action=raw&ctype=text/javascript' )// Backlink: [[User:Lopullinen/gadgets/permanentlink.js]] 
+mw.loader.load( '/w/index.php?title=User:和平奮鬥救地球/duplinks.js&action=raw&ctype=text/javascript' )// Backlink: [[User:和平奮鬥救地球/duplinks.js]] 
+mw.loader.load( '/w/index.php?title=m:User:Krinkle/Scripts/CVNSimpleOverlay_wiki.js&action=raw&ctype=text/javascript' )// Backlink: [[m:User:Krinkle/Scripts/CVNSimpleOverlay_wiki.js]]
+var difflink = window.difflink = window.difflink || ['編輯差異'];
+//above from oldid=66217946
